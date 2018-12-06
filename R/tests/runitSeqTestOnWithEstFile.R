adc774c4a39fb22b2004d9d31c900809519d2353
wangruibo <wangruibo@sxu.edu.cn>
2018-12-06 20:37:45 +0800

    update test script.

--- R/tests/runitSeqTestOnWithEstFile.R
+++ R/tests/runitSeqTestOnWithEstFile.R
@@ -75,20 +75,17 @@ testMx2BCVSeqTest_dietterich <- function(upper_m, delta, relative=F, file.name=N
 
 
 
-testMx2BCVSeqTest_bmx2cv <- function(upper_m, delta0, delta1, agr.cnt=1, relative=F, file.name=NULL, est.type="typeI", sim_count=NULL, alpha=0.05, beta=0.05, rho1=NULL, rho2=NULL) {
+testMx2BCVSeqTest_bmx2cv <- function(upper_m, delta, relative=F, file.name=NULL, est.type="typeI", sim_count=NULL, alpha=0.05, rho1=NULL, rho2=NULL) {
   source("./tasks/mx2cv_seq_significant_test.R", encoding="UTF-8")
   results <- c()
   conf_TASK_BMX2SEQTEST <- list(
     est_type = est.type,
     alpha = alpha,
-    beta  = beta,
-    agree_cnt = agr.cnt,
     relative = relative,
     var.est.conf = list(
       name = "var_est3_mx2cv"
     ),
-    delta_0 = delta0,
-    delta_1 = delta1,
+    delta = delta,
     upper_m = upper_m,
     lower_m = 3
   )
@@ -109,33 +106,28 @@ testMx2BCVSeqTest_bmx2cv <- function(upper_m, delta0, delta1, agr.cnt=1, relativ
     conf_TASK_BMX2SEQTEST$pre_vec <- as.numeric(muv)
     task_config <- mx2cv_seq_significant_test.task_config_validation(conf_TASK_BMX2SEQTEST)
     result      <- mx2cv_seq_significant_test.perform_task(task_config)
-    results     <- rbind(results, c(result$test.result[1,1],result$test.result[1,2],result$test.result[1,5],result$test.result[1,6]))
+    results     <- rbind(results, c(result$test.result[1,1],result$test.result[1,2]))
   }  
-  m.stop  <- upper_m + agr.cnt-1
+  m.stop  <- upper_m
   result.table <- c()
-  result.h0 <- table(factor(results[which(results[,2]==0),1], levels = conf_TASK_BMX2SEQTEST$lower_m:m.stop))
+  result.h0 <- table(factor(results[which(results[,2]==0),1], levels = conf_task_dietterich$lower_m:m.stop))
   result.table <- rbind(result.table, result.h0)
-  result.h1 <- table(factor(results[which(results[,2]==1),1], levels = conf_TASK_BMX2SEQTEST$lower_m:m.stop))
+  result.h1 <- table(factor(results[which(results[,2]==1),1], levels = conf_task_dietterich$lower_m:m.stop))
   result.table <- rbind(result.table, result.h1)
-  result.no <- table(factor(results[which(results[,2]==2),1], levels = conf_TASK_BMX2SEQTEST$lower_m:m.stop))
-  result.table <- rbind(result.table, result.no)
   props <- t(t(rowSums(result.table)/sim_count))
   colnames(props) <- c("prop")
   result.table <- cbind(result.table, props)
-  row.names(result.table) <- c("accept H0:", "accept H1:", "other:")
+  row.names(result.table) <- c("No Reject:", "Reject:")
   # 计算错误率
-  m.fixed <- computeExpectStopTime(c(result.h0), c(result.h1), c(result.no), sim_count)
+  m.fixed <- computeExpectStopTime(c(result.h0), c(result.h1), repi =  sim_count)
   conf_TASK_BMX2SEQTEST <- list(
     est_type = est.type,
     alpha = alpha,
-    beta  = beta,
-    agree_cnt = agr.cnt,
     relative = relative,
     var.est.conf = list(
       name = "var_est3_mx2cv"
     ),
-    delta_0 = delta0,
-    delta_1 = delta1,
+    delta = delta,
     upper_m = upper_m,
     fixed_m = m.fixed,
     lower_m = 3
@@ -153,25 +145,20 @@ testMx2BCVSeqTest_bmx2cv <- function(upper_m, delta0, delta1, agr.cnt=1, relativ
     result      <- mx2cv_seq_significant_test.perform_task(task_config)
     results     <- rbind(results, c(result$test.result[1,1],result$test.result[1,2],result$test.result[1,5],result$test.result[1,6]))
   }  
-  m.stop  <- upper_m + agr.cnt-1
+  m.stop  <- upper_m
   result.table.mfix <- c()
-  result.h0.mfix <- table(factor(results[which(results[,2]==0),1], levels = conf_TASK_BMX2SEQTEST$lower_m:m.stop))
+  result.h0.mfix <- table(factor(results[which(results[,2]==0),1], levels = conf_task_dietterich$lower_m:m.stop))
   result.table.mfix <- rbind(result.table.mfix, result.h0.mfix)
-  result.h1.mfix <- table(factor(results[which(results[,2]==1),1], levels = conf_TASK_BMX2SEQTEST$lower_m:m.stop))
+  result.h1.mfix <- table(factor(results[which(results[,2]==1),1], levels = conf_task_dietterich$lower_m:m.stop))
   result.table.mfix <- rbind(result.table.mfix, result.h1.mfix)
-  result.no.mfix <- table(factor(results[which(results[,2]==2),1], levels = conf_TASK_BMX2SEQTEST$lower_m:m.stop))
-  result.table.mfix <- rbind(result.table.mfix, result.no.mfix)
   props.mfix <- t(t(rowSums(result.table.mfix)/sim_count))
-  type1error <- mean(results[,3])
-  type2error <- mean(results[,4])
-  
-  format.result <- c(result.h0, result.h1, result.no, m.fixed, c(props), c(props.mfix), type1error, type2error)
+  format.result <- c(result.h0, result.h1, m.fixed, c(props),c(props.mfix))
   colnames(format.result) <- NULL
   names(format.result) <- NULL
   return(format.result)
 }
 
-testWithExternFileParallel_bmx2 <- function(delta.start, delta.end, delta.step, true.value, file.name, upper.m, est.type, dataset.tag, delta.equal=T, rho1=NULL, rho2=NULL) {
+testWithExternFileParallel_bmx2 <- function(delta.start, delta.end, delta.step, true.value, file.name, upper.m, est.type, dataset.tag, rho1=NULL, rho2=NULL) {
   library(foreach)
   library(doParallel)
   source("./tests/testsCollectDatasets.R", encoding="UTF-8")
@@ -179,44 +166,19 @@ testWithExternFileParallel_bmx2 <- function(delta.start, delta.end, delta.step, 
   delta.seq <- seq(delta.start, delta.end, delta.step)
   delta.seq <- c(delta.seq, true.value)
   delta.seq <- sort(delta.seq)
-  sim.count <- NULL
-  if(delta.equal == F)
-    sim.count <- length(delta.seq) * (length(delta.seq)+1)/2
-  else 
-    sim.count <- length(delta.seq)
-  m.expect <- NULL
+  sim.count <- length(delta.seq)
   cl <- makeCluster(type="MPI")
   registerDoParallel(cl)
   results <- foreach(i=1:sim.count, .combine = rbind, .export = ls(.GlobalEnv)) %dopar% {
     setwd(work.directory)
-    if(delta.equal == F) {
-      sum <- 0
-      k <- 0
-      for(j in 0:i) {
-        sum =sum+j
-        if(sum >= i) {
-          sum <- sum-j
-          j <- j-1
-          break
-        }
-        k <- k+1
-      }
-      delta1.index <-  k 
-      delta0.index <- i-sum
-      delta1 <- delta.seq[delta1.index]
-      delta0 <- delta.seq[delta0.index]
-    } else {
-      delta1 <- delta.seq[i]
-      delta0 <- delta.seq[i]
-    }
-    result <- NULL
-    result <- testMx2BCVSeqTest_bmx2cv(upper.m, delta0, delta1,  file.name=file.name, est.type=est.type, rho1=rho1, rho2=rho2)
+    delta <- delta.seq[i]
+    result <- testMx2BCVSeqTest_bmx2cv(upper.m, delta,  file.name=file.name, est.type=est.type, rho1=rho1, rho2=rho2)
     c(delta0, delta1, result)
   }
   write.csv(results, file = paste("est_for_test/", paste("bmx2", dataset.tag, est.type, format(Sys.time(), "%Y%m%d%H%M%S"),sep="_"), sep=""))
 }
 
-testWithExternFileParallel_diet <- function(delta.start, delta.end, delta.step, true.value, file.name, upper.m, dataset.tag, delta.equal=T) {
+testWithExternFileParallel_diet <- function(delta.start, delta.end, delta.step, true.value, file.name, upper.m, dataset.tag) {
   library(foreach)
   library(doParallel)
   source("./tests/testsCollectDatasets.R", encoding="UTF-8")
@@ -224,39 +186,13 @@ testWithExternFileParallel_diet <- function(delta.start, delta.end, delta.step, 
   delta.seq <- seq(delta.start, delta.end, delta.step)
   delta.seq <- c(delta.seq, true.value)
   delta.seq <- sort(delta.seq)
-  sim.count <- NULL
-  if(delta.equal == F)
-    sim.count <- length(delta.seq) * (length(delta.seq)+1)/2
-  else 
-    sim.count <- length(delta.seq)
+  sim.count <- length(delta.seq)
   cl <- makeCluster(type="MPI")
   registerDoParallel(cl)
-  m.expect <- NULL
   results <- foreach(i=1:sim.count, .combine = rbind, .export = ls(.GlobalEnv)) %dopar% {
     setwd(work.directory)
-    # 根据i换算出delta值
-    if(delta.equal == F) {
-      sum <- 0
-      k <- 0
-      for(j in 0:i) {
-        sum =sum+j
-        if(sum >= i) {
-          sum <- sum-j
-          j <- j-1
-          break
-        }
-        k <- k+1
-      }
-      delta1.index <-  k 
-      delta0.index <- i-sum
-      delta1 <- delta.seq[delta1.index]
-      delta0 <- delta.seq[delta0.index]
-    } else {
-      delta1 <- delta.seq[i]
-      delta0 <- delta.seq[i]
-    }
-    result <- NULL
-    result <- testMx2BCVSeqTest_dietterich(upper.m, delta0, delta1, file.name=file.name) 
+    delta <- delta.seq[i]
+    result <- testMx2BCVSeqTest_dietterich(upper.m, delta, file.name=file.name) 
     c(delta0, delta1, result)
   }
   write.csv(results, file = paste("est_for_test/", paste("diet", dataset.tag, format(Sys.time(), "%Y%m%d%H%M%S"),sep="_"), sep=""))
