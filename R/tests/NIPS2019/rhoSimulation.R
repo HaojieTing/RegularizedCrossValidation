85b6ddabf8dd8ffec8cda228205475d370ed2406
wangruibo <wangruibo@sxu.edu.cn>
2019-04-09 11:50:58 +0800

    Update rhoSimulation.R

--- R/tests/NIPS2019/rhoSimulation.R
+++ R/tests/NIPS2019/rhoSimulation.R
@@ -1,1 +1,23 @@
 # 模拟BRHS中的rho值。
+
+testCovarianceSimulation <- function() {
+  source("tasks/covariance_simulation.R", encoding = "UTF-8")  
+  config <- list(
+    rpt = 1000,
+    seq = FALSE,
+    dataset.conf = list(
+      
+    ),
+    algorithm1.conf = list(
+      
+    ),
+    algorithm2.conf = list(
+      
+    ),
+    crossvalidation.conf = list(
+      name = "hold_out"
+    )
+  )
+  covariance_simulation.task_config_validation(config)
+  covariance_simulation.perform_task(config)
+}
