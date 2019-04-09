ec53912434207a6b3c67b286813fd2beba7ee80e
wangruibo <wangruibo@sxu.edu.cn>
2019-04-09 22:08:27 +0800

    Update rhoSimulation.R

--- R/tests/NIPS2019/rhoSimulation.R
+++ R/tests/NIPS2019/rhoSimulation.R
@@ -8,16 +8,17 @@ testCovarianceSimulation <- function() {
     overlap_count = round(150/6),
     dataset.conf = list(
       name = "simWYneco2014Expr2",
+      type = "classification",
       n = 150
     ),
     algorithm1.conf = list(
-      name = "logisticRegr"
+      name = "logisticRegr",
+      type = "classification"
     ),
     crossvalidation.conf = list(
       name = "hold_out",
       prop = 0.5
     )
-    
   )
   covariance_simulation.task_config_validation(config)
   result <- covariance_simulation.perform_task(config)
