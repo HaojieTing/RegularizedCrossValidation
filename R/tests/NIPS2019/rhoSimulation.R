956aa849cb034b5e18a30bb51e54d4eb39c9b6c8
wangruibo <wangruibo@sxu.edu.cn>
2019-04-09 22:56:54 +0800

    add a rho simulation experiment.

--- R/tests/NIPS2019/rhoSimulation.R
+++ R/tests/NIPS2019/rhoSimulation.R
@@ -26,3 +26,33 @@ testCovarianceSimulation <- function(nsize) {
   print(paste("data set size:", nsize))
   print(result)
 }
+
+testCovarianceSimulationBinaryLetter <- function(nsize) {
+  source("tasks/covariance_simulation.R", encoding = "UTF-8")  
+  config <- list(
+    rpt = 100000,
+    seq = F,
+    overlap_count = round(nsize/6),
+    dataset.conf = list(
+      name = "uci_letter",
+      type = "classification",
+      binarize = T,
+      samplingConf = list(
+        n = nsize
+      )
+    ),
+    algorithm1.conf = list(
+      name = "tree",
+      type = "classification"
+    ),
+    crossvalidation.conf = list(
+      name = "hold_out",
+      n1 = round(nsize/2),
+      n = nsize
+    )
+  )
+  covariance_simulation.task_config_validation(config)
+  result <- covariance_simulation.perform_task(config)
+  print(paste("data set size:", nsize))
+  print(result)
+}
