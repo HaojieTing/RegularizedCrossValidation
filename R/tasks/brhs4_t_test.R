ee97a8ca9a8deeaecfb71502e52aef79dcf87ab3
Rambo <wangruibo@sxu.edu.cn>
2019-04-17 22:23:51 +0800

    init brhs4_t_test

---
+++ R/tasks/brhs4_t_test.R
@@ -0,0 +1,13 @@
+# 对比两个有监督算法性能的BRHS t检验，假设重复次数为4.
+# Author: Wang Ruibo
+# E-mail: wangruibo@sxu.edu.cn
+# Date: 2019/4/17
+
+brhs4_t_test.task_config_validation <- function(task_config) {
+  
+  
+}
+
+brhs4_t_test.perform_task <- function(task_config) {
+  
+}
